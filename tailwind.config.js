decc18e60d559e8a0a5326a6b7b865ec68155fec
seungmin2222 <wooju1701@hanmail.net>
2024-07-15 21:14:53 +0900

    💄 Design: ImageList.jsx 스타일 수정 및 tailwind.config.js 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
       userSelect: {
         none: 'none',
       },
+      backgroundImage: {
+        checkerboard: `linear-gradient(45deg, #ccc 25%, transparent 25%),
+                         linear-gradient(135deg, #ccc 25%, transparent 25%),
+                         linear-gradient(45deg, transparent 75%, #ccc 75%),
+                         linear-gradient(135deg, transparent 75%, #ccc 75%)`,
+      },
+      backgroundSize: {
+        checkerboard: '20px 20px',
+      },
+      backgroundPosition: {
+        checkerboard: '0 0, 10px 0, 10px -10px, 0px 10px',
+      },
     },
   },
   plugins: [],
