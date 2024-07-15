9002f9e2cc4e51d0ad5de68f126ee4c0cd2fca90
seungminlee <seungmin@seungminleeui-noteubug.local>
2024-07-16 00:47:52 +0900

    ✨ Feat: Toast Message 구현

---
+++ src/components/Toast.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+export const Toast = ({ id, message, onClose }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+    const timer = setTimeout(() => {
+      setVisible(false);
+      setTimeout(() => onClose(id), 300);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [id, onClose]);
+
+  return (
+    <div
+      className={`fixed top-4 left-1/2 transform -translate-x-1/2 bg-[#1f77b4] font-semibold text-white px-4 py-2 rounded shadow-lg transition-opacity duration-300 ${
+        visible ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      {message}
+    </div>
+  );
+};
