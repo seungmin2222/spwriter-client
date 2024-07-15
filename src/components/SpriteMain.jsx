fe19dd95c4f1377d5d3384ac43a250daaad9e39e
seungminlee <seungmin@seungminleeui-noteubug.local>
2024-07-15 14:15:51 +0900

    💄 Design: 정적 화면 css 수정

--- src/components/SpriteMain.jsx
+++ src/components/SpriteMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navbar } from './Navbar';
 import { SpriteEditor } from './SpriteEditor';
+import { Footer } from './Footer';
 
 export const SpriteMain = () => {
   return (
-    <main className="flex flex-col w-[70%] w-min-[955px] bg-[#ffffff] rounded-md shadow-md">
+    <main className="flex flex-col w-[72%] w-min-[955px] h-full bg-[#ffffff] rounded-md shadow-md">
       <Navbar />
       <SpriteEditor />
+      <Footer />
     </main>
   );
 };
