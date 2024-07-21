aa168f60a0302f19a54dd9aef9631e4f473327b7
seungmin2222 <wooju1701@hanmail.net>
2024-07-21 18:03:24 +0900

    ♻️ Refactor: store.js 사용하지않은 데이터 제거

--- store.js
+++ store.js
@@ -17,69 +17,8 @@ const useFileStore = create(set => ({
   selectedFiles: new Set(),
   setSelectedFiles: selectedFiles => set({ selectedFiles }),
 
-  toggleSelectedFile: file =>
-    set(state => {
-      const newSelectedFiles = new Set(state.selectedFiles);
-      if (newSelectedFiles.has(file)) {
-        newSelectedFiles.delete(file);
-      } else {
-        newSelectedFiles.add(file);
-      }
-      return { selectedFiles: newSelectedFiles };
-    }),
-
   fileName: '',
   setFileName: fileName => set({ fileName }),
-
-  cloneSelectedFiles: () => {
-    set(state => {
-      const { files, selectedFiles, coordinates } = state;
-
-      if (selectedFiles.size === 0) {
-        return;
-      }
-
-      const clonedImages = [];
-
-      selectedFiles.forEach(file => {
-        const fileIndex = coordinates.findIndex(coord => coord.img === file);
-        if (fileIndex === -1) {
-          return;
-        }
-
-        const coord = coordinates[fileIndex];
-        const newImg = new Image();
-        newImg.src = coord.img.src;
-
-        newImg.onload = () => {
-          const newCoord = {
-            img: newImg,
-            x: coord.x,
-            y: coord.y,
-            width: coord.width,
-            height: coord.height,
-          };
-
-          clonedImages.push(newCoord);
-
-          if (clonedImages.length === selectedFiles.size) {
-            const updatedFiles = [...files, ...clonedImages.map(c => c.img)];
-            const updatedCoordinates = [...coordinates, ...clonedImages];
-
-            sortAndSetCoordinates(updatedCoordinates, newCoordinates => {
-              set({
-                files: updatedFiles,
-                coordinates: newCoordinates,
-              });
-            });
-          }
-        };
-      });
-    });
-  },
-
-  updateCanvas: false,
-  setUpdateCanvas: update => set({ updateCanvas: update }),
 }));
 
 export default useFileStore;
