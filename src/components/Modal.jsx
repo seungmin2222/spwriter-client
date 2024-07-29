a18fc996f32d654c7db815d6a3105cf9b10d6ae0
seungminlee <seungmin@seungminleeui-noteubug.local>
2024-07-29 20:06:44 +0900

    💄 Design: Modal, ResizeModal 비슷하게 디자인 수정

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,58 +1,66 @@
 import React, { useEffect, useRef } from 'react';
 
 function Modal({ showModal, handleClose, handleConfirm, message }) {
-  const modalContentRef = useRef(null);
+  const modalContainerRef = useRef(null);
+  const confirmButtonRef = useRef(null);
 
   useEffect(() => {
-    if (showModal) {
-      modalContentRef.current.focus();
+    const handleKeyDown = e => {
+      if (e.key === 'Enter') {
+        handleConfirm();
+      } else if (e.key === 'Escape') {
+        handleClose();
+      }
+    };
+
+    if (showModal && modalContainerRef.current) {
+      modalContainerRef.current.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      if (modalContainerRef.current) {
+        modalContainerRef.current.removeEventListener('keydown', handleKeyDown);
+      }
+    };
+  }, [showModal, handleConfirm, handleClose]);
+
+  useEffect(() => {
+    if (showModal && confirmButtonRef.current) {
+      confirmButtonRef.current.focus();
     }
   }, [showModal]);
 
   if (!showModal) return null;
 
-  const handleBackgroundClick = () => {
-    handleClose();
-  };
-
-  const handleContentClick = e => {
-    e.stopPropagation();
-  };
-
-  const handleKeyDown = e => {
-    if (e.key === 'Enter') {
-      handleConfirm();
-    } else if (e.key === 'Escape') handleClose();
-  };
-
   return (
     <div
-      className="fixed inset-0 flex justify-center z-50 bg-black bg-opacity-50"
+      ref={modalContainerRef}
+      className="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto h-full w-full z-50 flex items-center justify-center"
       data-testid="modal"
-      onClick={handleBackgroundClick}
-      tabIndex={-1}
+      onClick={handleClose}
+      tabIndex="-1"
     >
       <div
-        className="relative top-96 w-[350px] h-[200px] bg-white rounded-lg shadow-lg p-6"
-        onClick={handleContentClick}
-        tabIndex={0}
-        ref={modalContentRef}
-        onKeyDown={handleKeyDown}
+        className="p-6 border w-80 shadow-lg rounded-[1rem] bg-white"
+        onClick={e => e.stopPropagation()}
       >
-        <h2 className="text-xl mb-5 text-gray-900 text-center">{message}</h2>
-        <div className="items-center px-4 py-3">
-          <button
-            onClick={handleConfirm}
-            className="px-4 py-2 bg-[#241f3a] text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-[#565465] focus:outline-none focus:ring-2 focus:ring-blue-300 duration-300"
-          >
-            확인
-          </button>
-          <button
-            onClick={handleClose}
-            className="mt-3 px-4 py-2 bg-[#f0f0f2] text-gray-700 text-base font-medium rounded-md w-full shadow-sm hover:bg-[#c9c7d2] focus:outline-none focus:ring-2 focus:ring-gray-300 duration-300"
-          >
-            취소
-          </button>
+        <div className="text-center">
+          <h3 className="text-xl leading-6 text-gray-900 mb-6">{message}</h3>
+          <div>
+            <button
+              ref={confirmButtonRef}
+              onClick={handleConfirm}
+              className="px-4 py-2 bg-[#241f3a] text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-[#565465] duration-300 mb-3"
+            >
+              확인
+            </button>
+            <button
+              onClick={handleClose}
+              className="px-4 py-2 bg-[#f0f0f2] text-gray-700 text-base font-medium rounded-md w-full shadow-sm hover:bg-[#c9c7d2] duration-300"
+            >
+              취소
+            </button>
+          </div>
         </div>
       </div>
     </div>
