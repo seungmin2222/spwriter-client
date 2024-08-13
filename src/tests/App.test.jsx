00d9c72592d72e832d79adcb5e8b840671993599
seungmin2222 <wooju1701@hanmail.net>
2024-08-13 15:03:04 +0900

    ✅ Test: 모바일 대응 테스트코드 추가 작성

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import App from '../components/App';
 
 describe('App component', () => {
+  const originalUserAgent = window.navigator.userAgent;
+
+  beforeEach(() => {
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value:
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      configurable: true,
+    });
+  });
+
+  afterEach(() => {
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value: originalUserAgent,
+      configurable: true,
+    });
+  });
+
   it('renders correctly', () => {
     render(<App />);
     expect(screen.getByTestId('app')).toBeInTheDocument();
@@ -17,5 +34,25 @@ describe('App component', () => {
   it('contains the SpriteMain component', () => {
     render(<App />);
     expect(screen.getByTestId('sprite-main')).toBeInTheDocument();
+  });
+
+  it('displays mobile message on mobile devices', () => {
+    Object.defineProperty(window.navigator, 'userAgent', {
+      value:
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+      configurable: true,
+    });
+
+    render(<App />);
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element.tagName.toLowerCase() === 'h1' &&
+          content.includes('죄송합니다.') &&
+          content.includes('모바일은 지원하지 않습니다.')
+        );
+      })
+    ).toBeInTheDocument();
+    expect(screen.getByAltText('xMarkIcon Icon')).toBeInTheDocument();
   });
 });
